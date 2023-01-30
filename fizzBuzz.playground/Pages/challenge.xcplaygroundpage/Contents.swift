eba1ece0dd42a6221691cea9f7defeb4cb769169
michael Lazenby <michaelazenby@gmail.com>
2023-01-30 00:45:57 -0800

    loop practice

---
+++ fizzBuzz.playground/Pages/challenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//loop through 1 to 1000
+//if a number is a multiple of 3 print Fizz
+//if a number if a multiple of 5 print buzz
+//3 & 5 print fizzbuzz
+//if neithe print the number
+
+for index in 1...100 {
+    if index.isMultiple(of: 3){
+        print("Fizz")
+    }else if index.isMultiple(of: 5){
+        print("Buzz")
+    }else if index.isMultiple(of: 3) && index.isMultiple(of: 5){
+        print("FizzBuzz")
+    } else {
+        print(index)
+    }
+}
+
